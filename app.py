9570ac432cc1da24880c34071788da47fd6d16dd
Mustafizur Rahman Hilaly <diluict@gmail.com>
2021-08-11 21:35:21 -0500

    UI

--- app.py
+++ app.py
@@ -1,16 +1,50 @@
-from flask import Flask
+import os
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 import model_initializer
+import constants as const
 import caption_generator as generator
 
 app = Flask(__name__)
 
 # initialize all the model during app startup
 model_initializer.initialize(app)
+app.secret_key = "1a2b3canine34"
+app.config['UPLOAD_FOLDER'] = const.UPLOAD_DIR
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and displayed below')
+        # return redirect(url_for('index', filename=filename))
+        caption = generator.generate(app, const.UPLOAD_DIR + "/" + filename)
+        return render_template('index.html', filename=filename, caption=caption)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif')
+        return redirect(request.url)
 
 
 if __name__ == '__main__':
--- model_initializer.py
+++ model_initializer.py
@@ -1,8 +1,7 @@
 def initialize(app):
     import constants as const
     import pickle
-    with open(const.TOKENIZER_FILE, 'rb') as tokenizer_file, \
-            open(const.MODEL_FILE, 'rb') as model_file:
+    from keras.models import load_model
+    with open(const.TOKENIZER_FILE, 'rb') as tokenizer_file:
         app.tokenizer = pickle.load(tokenizer_file)
-        app.model = pickle.load(model_file)
-
+    app.model = load_model(const.MODEL_FILE)
