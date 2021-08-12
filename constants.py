8e943c602acee696be5f03e4a6f9d76abd5491df
Faisal Ahmed <faisalbegins@gmail.com>
2021-08-12 12:43:19 -0500

    refactored codes

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 import os
 TOKENIZER_FILE = f'{os.environ.get("DATA_ROOT_DIR")}/tokenizer.pkl'
-MODEL_FILE = f'{os.environ.get("DATA_ROOT_DIR")}/model_20epochs_final.h5'
+MODEL_FILE = f'{os.environ.get("DATA_ROOT_DIR")}/{os.environ.get("MODEL_FILE")}'
 UPLOAD_DIR = f'{os.environ.get("UPLOAD_DIR")}'
 MAX_SEQ_LENGTH = 34
---
+++ static/uploads/readme.txt
@@ -0,0 +1,1 @@
+all the uploads goes here
